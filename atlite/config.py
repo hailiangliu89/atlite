f62db264d26c1515568f7cebe8c74be749c32fd7
hailiangliu89 <liuhailiang1989@gmail.com>
2019-04-16 13:19:45 +0200

    set cutout dir

--- atlite/config.py
+++ atlite/config.py
@@ -1,6 +1,6 @@
 #Dataset Settings
 gebco_path = '/srv/data/REatlas/gebco_08.nc'
-cutout_dir = '/srv/data/atlite/cutouts'
+cutout_dir = '/home/201402677/atlite/cutouts'
 ncep_dir = '/srv/data/CFSR'
 cordex_dir = '/home/vres-climate/data/cordex/RCP8.5'
 sarah_dir = '/home/vres-climate/data/sarah_v2'
